b53d2f5d0cd43d3764822254e08781f3366e4027
yellow <13132565021@163.com>
2022-04-20 12:06:46 +0800

    1

--- src/main/java/com/test/PdfConvert.java
+++ src/main/java/com/test/PdfConvert.java
@@ -25,56 +25,51 @@ public class PdfConvert {
 
         ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("threadName" + "-%d").build();
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(8, 10, 30, TimeUnit.MINUTES, new LinkedBlockingQueue<>(100), namedThreadFactory, new ThreadPoolExecutor.CallerRunsPolicy());
-
+        new File("src/main/resources/image/").mkdir();
         for (int k = 0; k < 20; k++) {
-            try (PDDocument document = PDDocument.load(new File("C:\\Users\\50317\\Desktop\\合同文件测试.pdf"));) {
+            try (PDDocument document = PDDocument.load(new File("src/main/resources/test.pdf"));) {
                 long convertStart = System.currentTimeMillis();
                 PDFRenderer pdfRenderer = new PDFRenderer(document);
                 int size = document.getNumberOfPages();
                 boolean isInit = false;
-                // 补丁,多线程时第一次调用有问题，
-//            if (!isInit) {
-//                pdfRenderer.renderImageWithDPI(0, 130, ImageType.RGB);
-//                isInit = true;
-//            }
-//                for (int i = 0; i < size; i++) {
-//                    int finalI = i;
-//                    String imageFileName = UUID.randomUUID().toString().replace("-", "") + "-" + finalI + ".jpg";
-//                    try (FileOutputStream fileOutputStream =
-//                                 new FileOutputStream("C:\\Users\\50317\\Desktop\\图片\\" + imageFileName);
-//                         ByteArrayOutputStream out = new ByteArrayOutputStream();) {
-//                        BufferedImage image = pdfRenderer.renderImageWithDPI(finalI, 130, ImageType.RGB);
-//                        ImageIOUtil.writeImage(image, "jpg", out);
-//                        // 保存图片
-//                        out.writeTo(fileOutputStream);
-//                    } catch (IOException e) {
-//                        throw e;
-//                    }
-//                }
-                List<CompletableFuture<Void>> futureList = new ArrayList<>();
                 for (int i = 0; i < size; i++) {
                     int finalI = i;
-                    CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
-                        String imageFileName = UUID.randomUUID().toString().replace("-", "") + "-" + finalI + ".jpg";
-                        try (FileOutputStream fileOutputStream =
-                                     new FileOutputStream("C:\\Users\\50317\\Desktop\\图片\\" + imageFileName);
-                             ByteArrayOutputStream out = new ByteArrayOutputStream();) {
-                            BufferedImage image = pdfRenderer.renderImageWithDPI(finalI, 130, ImageType.RGB);
-                            ImageIOUtil.writeImage(image, "jpg", out);
-                            // 保存图片
-                            out.writeTo(fileOutputStream);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }, threadPoolExecutor).exceptionally(e -> {
-                        LOG.error("pdf转image失败，失败原因:" + e.getMessage(), e);
-                        throw new RuntimeException();
-                    });
-                    futureList.add(future);
+                    String imageFileName = UUID.randomUUID().toString().replace("-", "") + "-" + finalI + ".jpg";
+                    try (FileOutputStream fileOutputStream =
+                                 new FileOutputStream("src/main/resources/image/" + imageFileName);
+                         ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+                        BufferedImage image = pdfRenderer.renderImageWithDPI(finalI, 130, ImageType.RGB);
+                        ImageIOUtil.writeImage(image, "jpg", out);
+                        // 保存图片
+                        out.writeTo(fileOutputStream);
+                    } catch (IOException e) {
+                        throw e;
+                    }
                 }
-                for (CompletableFuture<Void> future : futureList) {
-                    future.get();
-                }
+//                List<CompletableFuture<Void>> futureList = new ArrayList<>();
+//                for (int i = 0; i < size; i++) {
+//                    int finalI = i;
+//                    CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+//                        String imageFileName = UUID.randomUUID().toString().replace("-", "") + "-" + finalI + ".jpg";
+//                        try (FileOutputStream fileOutputStream =
+//                                     new FileOutputStream("src/main/resources/image/" + imageFileName);
+//                             ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+//                            BufferedImage image = pdfRenderer.renderImageWithDPI(finalI, 130, ImageType.RGB);
+//                            ImageIOUtil.writeImage(image, "jpg", out);
+//                            // 保存图片
+//                            out.writeTo(fileOutputStream);
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }, threadPoolExecutor).exceptionally(e -> {
+//                        LOG.error("pdf转image失败，失败原因:" + e.getMessage(), e);
+//                        throw new RuntimeException();
+//                    });
+//                    futureList.add(future);
+//                }
+//                for (CompletableFuture<Void> future : futureList) {
+//                    future.get();
+//                }
                 LOG.info("pdfConvertImgs ,convert-cost time:" + (System.currentTimeMillis() - convertStart));
             } catch (IOException e) {
                 e.printStackTrace();
